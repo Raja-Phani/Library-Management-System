90fbb254b4cb82b9097163f36a2853fcd992a1b9
Raja-Phani <74456215+Raja-Phani@users.noreply.github.com>
2020-11-14 18:20:34 +0530

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,15 @@
+package com.lms.main;
+
+import com.lms.library.Librarian;
+
+public class MainClass 
+{
+	public static void main(String[] args)
+	{
+		Librarian l1=new Librarian();
+		l1.run();
+		
+		System.out.println();
+	}
+
+}
